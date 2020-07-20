881ca2576e53e35279b7fd754c999bc655a608ed
Ben1152000 <benjamindarnell00@gmail.com>
2020-07-20 00:31:50 -0700

    Clean up data storage

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,11 +4,59 @@ let zTop = 10; // highest z-index
 let grid;
 let gridDimensions;
 
+let map = {
+  width: 0,
+  height: 0,
+  tiles: [],
+  sprites: {}
+}
+
 function startup() {
   grid = document.getElementById("grid");
   gridDimensions = {rows: 0, columns: 0};
+  loadMapFile("assets/maps/sample.json");
 }
 
+function loadMapFile(mapFile) {
+  var request = new XMLHttpRequest();
+  request.open("GET", mapFile);
+  request.send(null);
+  request.onreadystatechange = function() {
+    if ( request.readyState === 4 && request.status === 200 ) {
+      map = JSON.parse(request.responseText);
+      render();
+    }
+  }
+}
+
+function render() {
+  grid.querySelectorAll('*').forEach(node => node.remove());
+  renderGrid(map.width, map.height);
+  map.tiles.forEach(renderTile);
+  for(var id in map.sprites) { 
+    var sprite = map.sprites[id];
+    sprite.id = id;
+    renderSprite(sprite);
+  }
+}
+
+function renderGrid() {
+  grid.style.gridTemplateColumns = (parseInt(scale) + "px ").repeat(map.width);
+  grid.style.gridTemplateRows = (parseInt(scale) + "px ").repeat(map.height);
+  for (row = 0; row < map.height; row++) {
+    for (column = 0; column < map.width; column++) {
+      var cell = document.createElement("DIV");
+      cell.id = "cell-" + parseInt(row) + "-" + parseInt(column);
+      cell.classList.add("grid-item")
+      cell.innerHTML = parseInt(map.width * row + column);
+      grid.appendChild(cell);
+    }
+  }
+}
+
+/**
+ * Get map grid coordinates from screen position.
+ */
 function getGridCoords(position) {
   return {
     left: (grid.scrollLeft + position.left - grid.offsetLeft) / scale,
@@ -16,21 +64,41 @@ function getGridCoords(position) {
   };
 }
 
-function getGridCell(gridCoords) {
-  if (gridCoords.left < 0) 
-    gridCoords.left = 0;
-  if (gridCoords.left >= gridDimensions.columns) 
-    gridCoords.left = gridDimensions.columns - 1;
-  if (gridCoords.top < 0) 
-    gridCoords.top = 0;
-  if (gridCoords.top >= gridDimensions.rows) 
-    gridCoords.top = gridDimensions.rows - 1;
-  var gridCell = document.getElementById(
-    "cell-" + 
-    parseInt(Math.floor(gridCoords.top)) + "-" + 
-    parseInt(Math.floor(gridCoords.left))
-  )
-  return gridCell;
+function renderTile(tile) {
+  var cell = document.getElementById(
+    "cell-" + parseInt(tile.top) + "-" + parseInt(tile.left)
+  );
+  if (cell) {
+    var image = document.createElement("IMG");
+    image.classList.add("sprite");
+    image.classList.add("sprite-tile");
+    image.classList.add("crispy");
+    image.src = tile.source;
+    image.width = parseInt(tile.width * scale);
+    image.height = parseInt(tile.height * scale);
+    image.style.zIndex = zTop++;
+    makeNonDraggable(image);
+    cell.appendChild(image);
+  }
+}
+
+function renderSprite(sprite) {
+  var cell = document.getElementById(
+    "cell-" + parseInt(sprite.top) + "-" + parseInt(sprite.left)
+  );
+  if (cell) {
+    var image = document.createElement("IMG");
+    image.id = sprite.id;
+    image.classList.add("sprite");
+    image.classList.add("sprite-mobile");
+    image.classList.add("crispy");
+    image.src = sprite.source;
+    image.width = parseInt(sprite.width * scale);
+    image.height = parseInt(sprite.height * scale);
+    image.style.zIndex = zTop++;
+    makeDraggable(image);
+    cell.appendChild(image);
+  }
 }
 
 /**
@@ -65,12 +133,13 @@ function makeDraggable(element) { // https://www.w3schools.com/howto/howto_js_dr
 
   function closeDragElement() {
     var gridCoords = getGridCoords(element.getBoundingClientRect());
-    gridCoords.left += 0.5; gridCoords.top += 0.5;
-    getGridCell(gridCoords).appendChild(element); // move element to new cell
-    element.style.left = "-1px"; // reset sprite position
-    element.style.top = "-1px";
+    var column = Math.max(0, Math.min(Math.round(gridCoords.left), map.width - 1));
+    var row = Math.max(0, Math.min(Math.round(gridCoords.top), map.height - 1));
+    map.sprites[element.id].left = column;
+    map.sprites[element.id].top = row;
     document.onmouseup = null;
     document.onmousemove = null;
+    render();
   }
 }
 
@@ -98,40 +167,7 @@ function makeNonDraggable(element) {
   }
 }
 
-function addSprite(source, column, row, width, height, foreground) {
-  var cell = document.getElementById("cell-" + parseInt(row) + "-" + parseInt(column));
-  if (cell) {
-    var image = document.createElement("IMG");
-    image.classList.add("sprite");
-    if (foreground) image.classList.add("sprite-mobile");
-    image.classList.add("crispy");
-    image.src = source;
-    image.width = parseInt(width * scale);
-    image.height = parseInt(height * scale);
-    image.style.zIndex = zTop++;
-    if (foreground) makeDraggable(image);
-    else makeNonDraggable(image);
-    cell.appendChild(image);
-  }
-}
-
-function resizeGrid(rows, columns) {
-  gridDimensions.rows = rows;
-  gridDimensions.columns = columns;
-  grid.style.gridTemplateColumns = (parseInt(scale) + "px ").repeat(columns);
-  grid.style.gridTemplateRows = (parseInt(scale) + "px ").repeat(rows);
-  var i;
-  for (row = 0; row < rows; row++) {
-    for (column = 0; column < columns; column++) {
-      var cell = document.createElement("DIV");
-      cell.id = "cell-" + parseInt(row) + "-" + parseInt(column);
-      cell.classList.add("grid-item")
-      cell.innerHTML = parseInt(columns * row + column);
-      grid.appendChild(cell);
-    }
-  }
-}
-
+/*
 function loadMapFile(mapFile) {
   var request = new XMLHttpRequest();
   request.open("GET", mapFile);
@@ -151,3 +187,4 @@ function loadMapFile(mapFile) {
     }
   }
 }
+*/
