28a81a4c369883d428fa6f5979fa968bb313acd0
Ben1152000 <benjamindarnell00@gmail.com>
2020-07-28 03:03:44 -0700

    Fix bugs

--- assets/js/dom.js
+++ assets/js/dom.js
@@ -115,7 +115,7 @@ export class Dom {
   static displayAddSpriteWindow(type, callback) {
     $('#modal-add-sprite-submit').off('click');
     $('#modal-add-sprite-alert').attr('hidden', '');
-    $('#modal-add-sprite-source').val('');
+    /*$('#modal-add-sprite-source').val('');*/
     if (type === 'tile') {
       $('#toggle-frame-menu').attr('hidden', '');
       $('#color-select-menu').attr('hidden', '');
@@ -204,11 +204,10 @@ export class Dom {
 
   static selectSpriteColor(selection) {
     $('#color-select-menu').children().each((index, element) => {
+      $(element).empty();
       if ($(element).val() === selection) {
         $('#color-select-menu').data('color', selection);
         $(element).append($('<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-check2" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z"/></svg>'));
-      } else {
-        $(element).empty();
       }
     });
   }
